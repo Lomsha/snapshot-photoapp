e9d9b584250fd4e32e3dc0b8ca0bcb02596c88d3
Benshalom <benshalom.kariuki@kopokopo.com>
2022-02-24 12:39:26 +0000

    skip authenticate user to display the welcome page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]  #this skips the before action that  performs on all the controlllers, it skips it for the welcome controller index action (you don't need to be signed in to access the homepage)
   def index
   end
 end
