a1d327e14c6f9706f2f777db600880e44055d188
Benshalom <benshalom.kariuki@kopokopo.com>
2022-02-25 09:04:02 +0000

    update the payment model and user model method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+  has_one :payment  #the user will be required to make a payment once to sign up for the site
+  accepts_nested_attributes_for :payment #The user accepts nested attributes  for payments through the form submission also. 
+  #whenever we  sign up a user it's going to be handled by the registrations new form , in the form we are going to hit the user's table and payments table.
 end
